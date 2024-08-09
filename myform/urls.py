a0bebb958861227917e0b2f73a73f134110402b4
Aamir10-02 <aamir2saiyad@gmail.com>
2024-08-09 02:57:16 +0100

    Your descriptive commit message

--- myform/urls.py
+++ myform/urls.py
@@ -14,20 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-# from django.contrib import admin
-# from django.urls import path
-# from form import views
-# from form.views import form_submission_view
-
-# urlpatterns = [
-#     path('form', views.formApi, name='form'),
-#     path('form/<int:id>', views.formApi, name='form-details'),
-#     path('submit-form/', form_submission_view, name='submit_form'),
-#     path('admin/', admin.site.urls),
-# ]
-
-
 from django.urls import path
 from django.contrib import admin
 from form import views
