b1a1ff1e3e1e0051765d7d8b1f1efc6ccac0999b
Aamir10-02 <143529993+Aamir10-02@users.noreply.github.com>
2024-09-12 03:06:39 +0100

    Update urls.py

--- myform/urls.py
+++ myform/urls.py
@@ -17,9 +17,9 @@ from form import views
 from form.views import form_submission_view
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Root URL
-    path('form', views.formApi, name='form'),
-    path('form/<int:id>', views.formApi, name='form-details'),
+    path('', views.home, name='home'),
+    path('formApi/', views.formApi, name='form-api'),  # Changed the path to 'formApi'
+    path('formApi/<int:id>/', views.formApi, name='form-details'),
     path('admin/', admin.site.urls),
-    path('form/', form_submission_view, name='form_submission'),
+    path('form/', form_submission_view, name='form_submission'),  # This handles form submission
 ]
