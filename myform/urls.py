2b7f51a24d92987e8f53f936efbc6a634cae484e
Aamir10-02 <143529993+Aamir10-02@users.noreply.github.com>
2024-09-17 03:31:35 +0100

    Update urls.py

--- myform/urls.py
+++ myform/urls.py
@@ -14,12 +14,12 @@
 from django.urls import path
 from django.contrib import admin
 from form import views
-from form.views import form_submission_view
+
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('formApi/', views.formApi, name='form-api'),  # Changed the path to 'formApi'
     path('formApi/<int:id>/', views.formApi, name='form-details'),
     path('admin/', admin.site.urls),
-    path('form/', form_submission_view, name='form_submission'),  # This handles form submission
+    path('form/', views.form_submission_view, name='form_submission'),  # This handles form submission
 ]
